1e7e516ba681b852d21d15d71abc28b4a45eb92d
Nourah-242 <222511241@psu.edu.sa>
2026-05-07 00:15:58 +0300

    Create RouterRepository interface for router model

---
+++ assettrack-backend/src/main/java/com/assettrack/assettrack_backend/Repository/RouterRepository.java
@@ -0,0 +1,7 @@
+package com.assettrack.assettrack_backend.Repository;
+
+import com.assettrack.assettrack_backend.model.router;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RouterRepository extends JpaRepository<router, String> {
+}
